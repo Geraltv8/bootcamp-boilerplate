ec450b2160e8ccec402daad5a39c8dcfd361f1c1
Bauti <bauticapovilla9@gmail.com>
2026-08-06 19:50:20 -0300

    Modify app.js

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const errorHandlerMiddleware = require('./src/middlewares/errorHandler.middlewar
 
 const turnosRoutes = require('./src/routes/turnos.routes');
 const pacientesRoutes = require('./src/routes/paciente.routes');
-
+const consultoriosRoutes = require('./src/routes/consultorio.routes');
 app.use(express.json());
 app.use(auditMiddleware);
 
 app.use('/api/v1/turnos', turnosRoutes);
 app.use('/api/v1/pacientes', pacientesRoutes);
-
+app.use('/api/v1/consultorios', consultoriosRoutes);
 app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3000;
